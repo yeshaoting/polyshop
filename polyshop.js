4818c58008e523de63873813e57fd850bd64c28f
叶绍亭 <yeshaoting@meituan.com>
2016-09-10 12:48:46 +0800

    polyshop重构

--- polyshop.js
+++ polyshop.js
@@ -1,125 +1,159 @@
 
+var constants = {
+	"hostname": "polyshop.com.cn",
+	"check_url": 'http://polyshop.com.cn/index.php/Home/Ajax/updateprice.html',
+	"seckill_url": 'http://polyshop.com.cn/index.php/Home/Ajax/addPrice.html',
 
-var check_url = '/index.php/Home/Ajax/updateprice.html';
-//var check_url = 'http://localhost/polyshop/updateprice.json';
-console.info("check url: %s", check_url);
+	//要秒杀的房间id列表
+	"ids": [2260, 2261, 2262],
+
+	//秒杀活动是否开启
+	"isopen": false,
+
+	//1表示参与秒杀模式，0表示参与加价模式
+	"sh2": 1,
+	"interval": 250,
+
+	//当前秒杀房间结果
+	"ok": 0,
+
+	//当前秒杀的房间id
+	"current_id": 0,
+
+	//查看秒杀活动是否开启间隔
+	"check_interval": 200,
+
+	//秒杀抢购间隔
+	"seckill_interval": 250,
+};
 
 
-var seckill_url = '/index.php/Home/Ajax/addPrice.html';
-//var seckill_url = 'http://localhost/polyshop/addPrice.json';
-console.info("seckill url: %s", seckill_url);
+// 脚本参数初始化
+function constants_init() {
+	var hostname = $(location).attr('hostname')
+	console.info("hostname: %s", hostname);
+	if (hostname == "localhost") {
+		constants.check_url = 'http://localhost/polyshop/updateprice.json';
+		constants.seckill_url = 'http://localhost/polyshop/addPrice.json';
+	}
 
+	if (!$.isArray(constants.ids) || constants.ids.length == 0) {
+		console.info("请填写正确的秒杀房间id列表");
+		throw "请填写正确的秒杀房间id列表";
+	}
 
-$version = $().jquery;
-console.info("jquery version: %s", $version);
-
-//要秒杀的房间id
-var ids = [2260, 2261, 2262];
-console.info("all id ids: %o", ids);
-
-//秒杀活动是否开启
-var isopen = false;
-console.info("is open sec kill: %o", isopen);
-
-var sh2 = 1; // 1表示参与秒杀模式，0表示参与加价模式
-console.info("sec kill sh2: %d", sh2);
-
-var interval = 1000;
-var ok = 0;
-var current_id = ids[0];
-console.info("current id: %d", current_id);
-
-function start() {
-	console.info("启动秒杀脚本~");
-	bootstrap(ids);
+	constants.current_id = constants.ids[0];
 }
 
-function bootstrap(ids) {
-	for (var i in ids) {
-		var id = ids[i];
 
-		ok = 0;	
-		current_id = id;
-		seckill(id);
+// 脚本参数输出
+function constants_display() {
+	var version = $().jquery;
+	console.info("jquery version: $s, constants: %o", version, constants);
+	console.info("ids: %o", constants.ids);
+}
 
-		if (ok == 1) {
-			console.info("成功秒杀, id: %d", id);
-			break;
-		} else if (ok == -1) {
-			console.info("秒杀失败, id: %d", id);
+
+// 监听秒杀活动是否开启
+function listen_check() {
+	console.info("监听秒杀活动是否开启~");
+	var isopenCheck = setInterval(function() {
+		if (constants.isopen) {
+			console.warn("监听到秒杀活动已经开启~");
+
+			//seckill(constants.ids);
+			clearInterval(isopenCheck);
+			return;
 		}
 
+		console.log("查看秒杀活动状态~");
+		check_seckill(constants.current_id, constants.sh2);
+	}, constants.check_interval);
+}
+
+
+// 查看秒杀活动状态
+function check_seckill(id, sh2) {
+    $.ajax({
+        url: constants.check_url,
+        data: {id: id},
+        type: 'post',
+        dataType: 'json',
+        async: true,
+        success: function (data) {
+        	if (data.s == 4) {
+        		console.info("秒杀活动正式开启~");
+        		console.warn("秒杀活动正式开启~");
+
+        		seckill();
+        		return;
+        	} else if (data.s == 1) {
+        		console.log("更新竞价价格");
+                return;
+            } else if (data.s == -1) {
+                console.log('你已经没有选房的机会啦');
+
+            	seckill();
+                return false;
+            } else if (data.s == 2) {
+                console.log('竞价已结束');
+            	
+            	seckill();
+                return false;
+            } else if (data.s == 3) {
+                console.log('竞价未开始');
+
+                constants.isopen = false;
+                return false;
+            }
+        	
+
+        	console.info("秒杀活动尚未开启~");
+        },
+        error: function (data) {
+        	console.error("exception: %o", data);
+        	sleep(interval);
+        }
+    });
+
+}
+
+
+// 排队秒杀
+function seckill() {
+	constants.isopen = true;
+	for (var idx = 0; idx < constants.ids.length; idx++) {
+		constants.ok = 0;
+
+		var id = constants.ids[idx];
+		constants.current_id = id;
+		doseckill(id, constants.sh2);
+
+		if (constants.ok == 1) {
+			console.info("成功秒杀id: %d", id);
+			break;
+		} else if (constants.ok == -1) {
+			console.info("秒杀失败id: %d", id);
+		}
 	}
 
-	console.info("finish~~");
+	console.info("秒杀脚本停止~~");
 }
 
-function seckill(id) {
-	console.log("准备秒杀id: %d, sh2: %d", id, sh2);
 
-	check(id, sh2);
-
-	doseckill(id, sh2);
-
-	return;
-}
-
-function check(id, sh2) {
-	while (!isopen) {
-	    $.ajax({
-	        url: check_url,
-	        data: {id: id},
-	        type: 'post',
-	        dataType: 'json',
-	        async: false,
-	        success: function (data) {
-	        	if (data.s == 4) {
-	        		console.info("秒杀活动正式开启~");
-	        		console.warn("秒杀活动正式开启~");
-	        		isopen = true;
-
-	        		doseckill(id, sh2);
-	        		return;
-	        	} else if (data.s == 1) {
-                    return;
-                } else if (data.s == -1) {
-                	isopen = true;
-                    console.log('你已经没有选房的机会啦');
-                    return false;
-                } else if (data.s == 2) {
-                	isopen = true;
-                    console.log('竞价已结束');
-                    return false;
-                } else if (data.s == 3) {
-                    console.log('竞价未开始');
-                    isopen = false;
-                    return false;
-                }
-	        	
-
-	        	console.info("秒杀活动尚未开启~");
-	        },
-	        error: function (data) {
-	        	console.error("exception: %o", data);
-	        	sleep(interval);
-	        }
-	    });
-
-	}
-
-	return true;
-}
-
+// 单个秒杀
 function doseckill(id, sh2) {
-	if (!isopen) {
+	console.log("准备秒杀id: %d, sh2: %d", id, sh2);
+	if (!constants.isopen) {
 		console.info("秒杀活动尚未开始~");
 		return;
 	}
 
 	console.info("正在秒杀id: %d", id);
-	while (ok == 0) {
+
+	var seckkill_interval = setInterval(function() {
 		$.ajax({
-		    url: seckill_url,
+		    url: constants.seckill_url,
 		    data: {id: id,sh:sh2},
 		    type: 'post',
 		    async: false,
@@ -129,16 +163,12 @@ function doseckill(id, sh2) {
 		        	console.info("竞拍成功id: %d", id);
 		        	console.warn("竞拍成功id: %d", id);
 		            $('#alert4').show();
-		            ok = 1;
+		            constants.ok = 1;
 		            return;
 		        }
 
 		        if (data.s == 1) {
-		            // max_one_price=data.price;
-		            // $("#maxprice").html(max_one_price.toFixed(2)+"元");
-		            // maxdj=data.price/mj;
-
-		            // $("#maxunitprice").html(parseInt(maxdj) + "元");
+		            console.info("更新竞价价格");
 		        } else if (data.s == -1) {
 		            console.info('你已经没有选房的机会啦');
 		            changeStatus(id);
@@ -163,12 +193,14 @@ function doseckill(id, sh2) {
 		});
 
 		//sleep(interval);
-	}
+	}, constants.seckill_interval);
+
 }
 
+// 更改秒杀状态
 function changeStatus(id) {
-	if (id == current_id) {
-		ok = -1;
+	if (id == constants.current_id) {
+		constants.ok = -1;
 	}
 }
 
@@ -180,17 +212,33 @@ function sleep(milliSeconds){
 }
 
 
-$("#pricelog").click(function() {
+// 启动秒杀脚本
+function start() {
+	console.info("启动秒杀脚本~");
+
+	constants_init();
+	constants_display();
+
+	listen_check();
+	seckill();
+
+}
+
+// 启动秒杀脚本
+function stop() {
+	console.info("停止秒杀脚本~");
+	constants.ok = -1;
+}
+
+$("#pricelog").on("click", function() {
 	start();
 });
 
-$(".wrapper .content2 .mainTitle").click(function() {
-	isopen = true;
-	bootstrap(ids);
+$(".wrapper .content2 .mainTitle").on("click", function() {
+	seckill();
 });
 
-$("header div.wrapper h1.fl").click(function() {
-	isopen = true;
-	bootstrap(ids);
+$("header div.wrapper h1.fl").on("click", function() {
+	seckill();
 });
 
